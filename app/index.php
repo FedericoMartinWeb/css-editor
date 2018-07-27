1b23512e6c597aadfa73304e735d14f82ed40662
Federico Palacios <xxxxx@apple.com>
2018-07-27 18:37:02 -0300

    Start UI

--- app/index.php
+++ app/index.php
@@ -13,121 +13,53 @@
     <link rel="stylesheet" href="../app/temp/styles/styles.css">
     
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Vast+Shadow" rel="stylesheet">
     
     <!-- Icons -->
-    <link href="https://file.myfontastic.com/Fe6YFffCEVhcvQVzVyqwRm/icons.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Carter+One" rel="stylesheet">
     
     <!-- HTML5 shiv and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-
-    <style>
-        input{
-            cursor: pointer;
-            margin-top: 50px;
-        }
-
-        p{
-            margin: 0;
-            padding: 0;
-            color: #fff;
-            text-transform: uppercase;
-            letter-spacing: 1px;
-            font-family: Arial;
-        }
-
-        .box{
-            display: -webkit-flex;
-            display: -moz-flex;
-            display: -ms-flex;
-            display: -o-flex;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        
-        .box-wrapper{
-            display: -webkit-flex;
-            display: -moz-flex;
-            display: -ms-flex;
-            display: -o-flex;
-            display: flex;
-            flex-wrap: wrap;
-        }
-
-        .box{
-            width: 150px;
-            height: 150px;
-            background-color: green;
-            margin-top: 50px;
-            margin-left: 25px;
-        }
-
-        @media only screen and (min-width: 768px){
-            .box:first-child{
-                margin-left: 0;
-            }
-        }
-
-        .green{
-            background-color: green;
-        }
-
-        .contador{
-            margin-top: 50px;
-            width: 200px;
-            height: 200px;
-            background-color: blue;
-            display: -webkit-flex;
-            display: -moz-flex;
-            display: -ms-flex;
-            display: -o-flex;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            color: #fff;
-            font-size: 45px;
-            font-family: arial;
-        }
-
-        .modifi{
-            width: 100px;
-            height: 100px;
-            background-color: green;
-            margin-top: 50px;
-        }
-
-        .slider{
-            position: fixed;
-            bottom: 50px;
-            right: 50px;
-        }
-
-        .blur{
-            position: fixed;
-            bottom: 50px;
-            left: 50px;
-        }
-
-        .props{
-            position: fixed;
-            top: 0;
-            right: 0;
-            height: 200px;
-            width: 500px;
-            background-color: red;
-            color: #fff;
-        }
-
-    </style>
 	
 </head>
 <body>
     
-    <div class="box-wrapper">
+    <!-- Header Start -->
+    <header class="site-header">
+        <h1 class="site-header__title">Css Editor!</h1>
+    </header>
+    <!-- Header End -->
+
+    <main class="main-wrapper">
+        <aside class="main-wrapper__aside">
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+            <div class="items">asdas</div>
+        </aside>
+
+        <article class="main-wrapper__img">
+            <img src="assets/images/dog.jpg" alt="">
+        </article>
+    </main>
+
+    <!-- <div class="box-wrapper">
         <div class="box"><p class="parrafo"></p></div>
     </div>
 
@@ -136,15 +68,16 @@
 
     <div class="contador">0</div>
     <div class="modifi"></div>
+    <img src="assets/images/dog.jpg" class="modifi" alt="">
     <input type="range" min="-100" max="100" value="0" class="slider" id="myRange">
     <input type="color" value="#ff0000" class="color">
     <input type="range" value="0" max="50" class="blur">
 
     <div class="props"></div>
     <input type="submit" class="propbutton" value="mostrar css">
-    
+     -->
     <!-- End Footer -->
-     <script src="temp/scripts/App.js"></script>
+     <!-- <script src="temp/scripts/App.js"></script> -->
    
 </body>
 </html>
